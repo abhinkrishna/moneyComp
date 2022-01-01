b724575461306b148e277e1fd88526906f459f92
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2022-01-02 00:37:10 +0530

    new structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:cash_tracker/src/config/themes.dart';
-import 'package:cash_tracker/src/pages/home.dart';
+import 'package:cash_tracker/services/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -12,13 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    var materialTheme = ConfigTheme();
-
     return GetMaterialApp(
       title: dotenv.env['APP_NAME'] ?? '',
-      theme: materialTheme.appTheme,
-      home: HomePage(),
+      home: MainView(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/src/config/spacing.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ConfigSpacing {
-
-  final EdgeInsets mainTile = EdgeInsets.fromLTRB(15, 10, 15, 10);
-
-}
